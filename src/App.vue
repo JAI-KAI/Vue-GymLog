a2c875a618bf30cbd39e39bd4a9c508361fcb564
JAI-KAI <good003047@gmail.com>
2026-01-09 16:31:48 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="min-h-screen bg-gray-900 text-gray-100 p-6 flex-1">
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="mb-8 mt-4 text-center">
+        <h1 class="text-3xl font-black text-blue-500 italic uppercase tracking-wider">GymLog</h1>
+        <p class="text-gray-400 text-sm mt-1">選一個部位開始紀錄</p>
+    </header>
+    <PartButton :parts="parts"/>
+</template>
+
+<script setup lang="ts">
+import PartButton from '@/components/PartButton.vue';
+
+const parts = ['胸', '背', '腿', '功能']
+
+</script>
